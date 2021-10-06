7e183e6c1efc818337a43244ff5b811d2d7edb0f
Alec Miller <alecashmiller@gmail.com>
2021-10-06 19:31:35 -0400

    add everything

--- etch.js
+++ etch.js
@@ -1,12 +1,41 @@
-let div;
+
 const container = document.querySelector('#container');
 
+
 for(i = 0; i < 256; i++) {
-    let box = document.createElement('div');
-    box.textContent = "hi";
+    box = document.createElement('div');
+    box.className += "box";
     container.appendChild(box);
-    
 }
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    function randomColor() {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        let rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+        box.style.backgroundColor = rgb;
+    }
+    box.addEventListener('mouseover', () => {
+        
+        randomColor();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
